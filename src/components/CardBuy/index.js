b736104f297926738b626e40ed620430b8153323
ramBoson <ramamugesh95@gmail.com>
2021-08-06 13:52:40 +0530

    remove wait and print algo

--- src/components/CardBuy/index.js
+++ src/components/CardBuy/index.js
@@ -194,57 +194,57 @@ useEffect(()=>{usernameget()},[])
       //algo transfer
       //start money transfer here and opt and transfer algos
 
-    const waitForConfirmation = async function (algodclient, txId) {
-      let response = await algodclient.status().do();
-      let lastround = response["last-round"];
-      while (true) {
-          const pendingInfo = await algodclient.pendingTransactionInformation(txId).do();
-          if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
-              //Got the completed Transaction
-              console.log("Transaction " + txId + " confirmed in round " + pendingInfo["confirmed-round"]);
-              break;
-          }
-          lastround++;
-          await algodclient.statusAfterBlock(lastround).do();
-      }
-    };
+    // const waitForConfirmation = async function (algodclient, txId) {
+    //   let response = await algodclient.status().do();
+    //   let lastround = response["last-round"];
+    //   while (true) {
+    //       const pendingInfo = await algodclient.pendingTransactionInformation(txId).do();
+    //       if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+    //           //Got the completed Transaction
+    //           console.log("Transaction " + txId + " confirmed in round " + pendingInfo["confirmed-round"]);
+    //           break;
+    //       }
+    //       lastround++;
+    //       await algodclient.statusAfterBlock(lastround).do();
+    //   }
+    // };
     
     
     // Function used to print created asset for account and assetid
-    const printCreatedAsset = async function (algodclient, account, assetid) {
-      // note: if you have an indexer instance available it is easier to just use this
-      //     let accountInfo = await indexerClient.searchAccounts()
-      //    .assetID(assetIndex).do();
-      // and in the loop below use this to extract the asset for a particular account
-      // accountInfo['accounts'][idx][account]);
-      let accountInfo = await algodclient.accountInformation(account).do();
-      for (let idx = 0; idx < accountInfo['created-assets'].length; idx++) {
-          let scrutinizedAsset = accountInfo['created-assets'][idx];
-          if (scrutinizedAsset['index'] === assetid) {
-              console.log("AssetID = " + scrutinizedAsset['index']);
-              let myparms = JSON.stringify(scrutinizedAsset['params'], undefined, 2);
-              console.log("parms = " + myparms);
-              break;
-          }
-      }
-    };
+    // const printCreatedAsset = async function (algodclient, account, assetid) {
+    //   // note: if you have an indexer instance available it is easier to just use this
+    //   //     let accountInfo = await indexerClient.searchAccounts()
+    //   //    .assetID(assetIndex).do();
+    //   // and in the loop below use this to extract the asset for a particular account
+    //   // accountInfo['accounts'][idx][account]);
+    //   let accountInfo = await algodclient.accountInformation(account).do();
+    //   for (let idx = 0; idx < accountInfo['created-assets'].length; idx++) {
+    //       let scrutinizedAsset = accountInfo['created-assets'][idx];
+    //       if (scrutinizedAsset['index'] === assetid) {
+    //           console.log("AssetID = " + scrutinizedAsset['index']);
+    //           let myparms = JSON.stringify(scrutinizedAsset['params'], undefined, 2);
+    //           console.log("parms = " + myparms);
+    //           break;
+    //       }
+    //   }
+    // };
     // Function used to print asset holding for account and assetid
-    const printAssetHolding = async function (algodclient, account, assetid) {
-      // note: if you have an indexer instance available it is easier to just use this
-      //     let accountInfo = await indexerClient.searchAccounts()
-      //    .assetID(assetIndex).do();
-      // and in the loop below use this to extract the asset for a particular account
-      // accountInfo['accounts'][idx][account]);
-      let accountInfo = await algodclient.accountInformation(account).do();
-      for (let idx = 0; idx < accountInfo['assets'].length; idx++) {
-          let scrutinizedAsset = accountInfo['assets'][idx];
-          if (scrutinizedAsset['asset-id'] === assetid) {
-              let myassetholding = JSON.stringify(scrutinizedAsset, undefined, 2);
-              console.log("assetholdinginfo = " + myassetholding);
-              break;
-          }
-      }
-    };
+    // const printAssetHolding = async function (algodclient, account, assetid) {
+    //   // note: if you have an indexer instance available it is easier to just use this
+    //   //     let accountInfo = await indexerClient.searchAccounts()
+    //   //    .assetID(assetIndex).do();
+    //   // and in the loop below use this to extract the asset for a particular account
+    //   // accountInfo['accounts'][idx][account]);
+    //   let accountInfo = await algodclient.accountInformation(account).do();
+    //   for (let idx = 0; idx < accountInfo['assets'].length; idx++) {
+    //       let scrutinizedAsset = accountInfo['assets'][idx];
+    //       if (scrutinizedAsset['asset-id'] === assetid) {
+    //           let myassetholding = JSON.stringify(scrutinizedAsset, undefined, 2);
+    //           console.log("assetholdinginfo = " + myassetholding);
+    //           break;
+    //       }
+    //   }
+    // };
     
     
     // var account1_mnemonic = "canal enact luggage spring similar zoo couple stomach shoe laptop middle wonder eager monitor weather number heavy skirt siren purity spell maze warfare ability ten";
@@ -293,13 +293,15 @@ useEffect(()=>{usernameget()},[])
         let opttx = (await algodclient.sendRawTransaction(rawSignedTxn).do());
         console.log("Transaction : " + opttx.txId);
         // wait for transaction to be confirmed
-        await waitForConfirmation(algodclient, opttx.txId);
+        //await waitForConfirmation(algodclient, opttx.txId);
     
         //You should now see the new asset listed in the account information
         console.log("Account 3 = " + recoveredAccount3.addr);
-        await printAssetHolding(algodclient, recoveredAccount3.addr, assetID);
+        //await printAssetHolding(algodclient, recoveredAccount3.addr, assetID);
     
     
+        console.log("opt","success")
+
         //trans
     
         sender = recoveredAccount1.addr;
@@ -321,12 +323,13 @@ useEffect(()=>{usernameget()},[])
      let xtx = (await algodclient.sendRawTransaction(rawSignedTxn).do());
      console.log("Transaction : " + xtx.txId);
      // wait for transaction to be confirmed
-     await waitForConfirmation(algodclient, xtx.txId);
+     //await waitForConfirmation(algodclient, xtx.txId);
     
      // You should now see the 10 assets listed in the account information
      console.log("Account 3 = " + recoveredAccount3.addr);
-     await printAssetHolding(algodclient, recoveredAccount3.addr, assetID);
+     //await printAssetHolding(algodclient, recoveredAccount3.addr, assetID);
 
+     console.log("trans","success")
 
      fireDb.database().ref(`imagerefexploreoneAlgos/${item.bid}`).child(item.highestBid).remove().then(()=>{
 
